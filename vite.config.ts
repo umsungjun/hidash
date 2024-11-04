508bbb947efe9067469ee27c4879aa04a5ced986
yceffort <yc.effort@navercorp.com>
2024-11-04 13:56:32 +0900

    [#1] 🔧 vite-plugin-dts 로 통일

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import {babel} from '@rollup/plugin-babel'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
 import preserveDirectives from 'rollup-preserve-directives'
 import {defineConfig} from 'vite'
+import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-import moduleMap from './index'
-
 const SUPPORT_TARGETS = browserslistToEsbuild()
+
+const moduleMap = {
+    isEmpty: './src/isEmpty.ts',
+    size: './src/size.ts',
+    keys: './src/keys.ts',
+    isNil: './src/isNil.ts',
+} as const
 
 export default defineConfig({
     plugins: [
@@ -27,6 +33,9 @@ export default defineConfig({
             ],
             extensions: ['.js', '.jsx', '.ts', '.tsx'],
             exclude: 'node_modules/**',
+        }),
+        dts({
+            rollupTypes: true,
         }),
     ],
     build: {
