83e90d099d13c6fb30bb4941b4a63f0441387fc1
yceffort <yc.effort@navercorp.com>
2024-11-22 16:54:10 +0900

    [#125] 🔧merge bench

--- src/shuffle.bench.ts
+++ src/shuffle.bench.ts
@@ -3,167 +3,49 @@ import {bench, describe} from 'vitest'
 
 import shuffle from './shuffle'
 
+const testCases = [
+    null,
+    undefined,
+    '',
+    'hello',
+    [],
+    [1, 2, 3],
+    {},
+    {key: 'value'},
+    new Map(),
+    new Set(),
+    new Map([
+        ['a', 1],
+        ['b', 2],
+    ]),
+    new Set([1, 2, 3]),
+    {a: 1, b: 2, c: 3},
+    Array(1000).fill(0),
+    'a'.repeat(1000),
+    new Array(1000)
+        .fill(0)
+        .map((_, i) => [`key${i}`, i])
+        .reduce((acc, [k, v]) => ({...acc, [k]: v}), {}),
+] as const
+
 const ITERATIONS = 1000
 
 describe('shuffle performance', () => {
-    describe('edge cases', () => {
-        const edgeCases = [null, undefined, '', {}]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                edgeCases.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                edgeCases.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
+    bench('hidash', () => {
+        for (let i = 0; i < ITERATIONS; i++) {
+            testCases.forEach((testCase) => {
+                shuffle(testCase)
+            })
+        }
     })
 
-    describe('small arrays', () => {
-        const arrays = [[], [1], [1, 2, 3], Array(10).fill(0)]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                arrays.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                arrays.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
-    })
-
-    describe('strings', () => {
-        const strings = ['hello', '한글테스트', 'a'.repeat(100)]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                strings.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                strings.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
-    })
-
-    describe('objects', () => {
-        const objects = [
-            {a: 1, b: 2},
-            Object.fromEntries(Array.from({length: 10}, (_, i) => [`key${i}`, i])),
-            {length: 3, 0: 'a', 1: 'b', 2: 'c'},
-        ]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                objects.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                objects.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
-    })
-
-    describe('Maps and Sets', () => {
-        const collections = [
-            new Map([
-                ['a', 1],
-                ['b', 2],
-            ]),
-            new Map(Array.from({length: 10}, (_, i) => [`key${i}`, i])),
-            new Set([1, 2, 3]),
-            new Set(Array.from({length: 10}, (_, i) => i)),
-        ]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                collections.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                collections.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
-    })
-
-    describe('large collections', () => {
-        const largeCollections = [
-            Array.from({length: 10000}, (_, i) => i),
-            'x'.repeat(10000),
-            new Map(Array.from({length: 1000}, (_, i) => [`key${i}`, i])),
-            new Set(Array.from({length: 1000}, (_, i) => i)),
-            Object.fromEntries(Array.from({length: 1000}, (_, i) => [`key${i}`, i])),
-        ]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                largeCollections.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                largeCollections.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
-    })
-
-    describe('array-like objects', () => {
-        const arrayLikes = [
-            {length: 3, 0: 'a', 1: 'b', 2: 'c'},
-            {length: 100, ...Array.from({length: 100}, (_, i) => [i, `value${i}`])},
-        ]
-
-        bench('hidash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                arrayLikes.forEach((testCase) => {
-                    shuffle(testCase)
-                })
-            }
-        })
-
-        bench('lodash shuffle', () => {
-            for (let i = 0; i < ITERATIONS; i++) {
-                arrayLikes.forEach((testCase) => {
-                    _shuffle(testCase)
-                })
-            }
-        })
+    bench('lodash', () => {
+        for (let i = 0; i < ITERATIONS; i++) {
+            testCases.forEach((testCase) => {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                _shuffle(testCase)
+            })
+        }
     })
 })
