5dbf333ad50cbb4697589fdc19ce2c6ed92f2d24
yujeong-jeon <yujeong.jeon@navercorp.com>
2024-11-29 00:01:36 +0900

    [#87] 🚀 findIndex

---
+++ src/findIndex.ts
@@ -0,0 +1,26 @@
+import {baseIteratee, ListIterateeCustom} from './internal/baseIteratee'
+
+export function findIndex<T>(array: T[], predicate?: ListIterateeCustom<T, unknown>, fromIndex?: number) {
+    const length = array.length
+
+    if (!length) {
+        return -1
+    }
+
+    const from = fromIndex !== undefined ? fromIndex : 0
+    const startIndex = Math.max(from >= 0 ? from : length + from, 0)
+
+    const iteratee = baseIteratee(predicate)
+
+    let index = startIndex - 1
+
+    while (++index < length) {
+        if (iteratee(array[index], index, array)) {
+            return index
+        }
+    }
+
+    return -1
+}
+
+export default findIndex
