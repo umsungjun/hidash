224ef6ae27c95b2998c95b7f9907455e0468812f
yceffort <yc.effort@navercorp.com>
2024-12-03 17:30:04 +0900

    [#69] 🔧 baseIteratee

--- src/uniqBy.ts
+++ src/uniqBy.ts
@@ -1,16 +1,18 @@
+import {baseIteratee, ListIteratee} from './internal/baseIteratee'
 import {List} from './internal/types'
 
-export function uniqBy<T, K>(array: List<T> | null | undefined, iteratee: (value: T) => K): T[] {
+export function uniqBy<T>(array: List<T> | null | undefined, iteratee: ListIteratee<T>): T[] {
     if (!array || !('length' in array)) {
         return []
     }
 
-    const seen = new Map<K, boolean>()
+    const seen = new Map<unknown, boolean>()
     const result: T[] = []
+    const iterateeFn = baseIteratee(iteratee)
 
     for (let i = 0; i < array.length; i++) {
         const value = array[i]
-        const computed = iteratee(value)
+        const computed = iterateeFn(value, i, array)
 
         if (!seen.has(computed)) {
             seen.set(computed, true)
