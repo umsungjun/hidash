d78573bc8a1eb84f6b3c2c067b7137bc2164ca7d
yceffort <yc.effort@navercorp.com>
2024-12-09 11:17:32 +0900

    [#72] 🔥

--- src/trim.bench.ts
+++ src/trim.bench.ts
@@ -4,31 +4,28 @@ import {bench, describe} from 'vitest'
 import trim from './trim'
 
 const testCases = [
-    // 공백 제거
-    {value: '  hello  '}, // 양쪽 공백
-    {value: '\tworld\t'}, // 탭 문자
-    {value: '\ntrim me\n'}, // 개행 문자
-    {value: ' no-space'}, // 앞 공백만
-    {value: 'space-only '}, // 뒤 공백만
-    {value: '   mixed spaces \t \n'}, // 혼합 공백
-    {value: ''}, // 빈 문자열
-    {value: '    '}, // 공백만 있는 문자열
+    {value: '  hello  '},
+    {value: '\tworld\t'},
+    {value: '\ntrim me\n'},
+    {value: ' no-space'},
+    {value: 'space-only '},
+    {value: '   mixed spaces \t \n'},
+    {value: ''},
+    {value: '    '},
 
-    // 커스텀 문자 제거
-    {value: ['--hello--', '-']}, // 대시(-)
-    {value: ['__world__', '_']}, // 밑줄(_)
-    {value: ['##test##', '#']}, // 해시(#)
-    {value: ['!!important!!', '!']}, // 느낌표(!)
-    {value: ['abcdeabc', 'abc']}, // 복잡한 문자 패턴
-    {value: ['---test--', '-']}, // 대시(-)
-    {value: ['xxtrimxx', 'x']}, // 문자(x)
+    {value: ['--hello--', '-']},
+    {value: ['__world__', '_']},
+    {value: ['##test##', '#']},
+    {value: ['!!important!!', '!']},
+    {value: ['abcdeabc', 'abc']},
+    {value: ['---test--', '-']},
+    {value: ['xxtrimxx', 'x']},
 
-    // 특수 케이스
-    {value: [null, undefined]}, // null과 undefined 처리
-    {value: ['$$$hello$$$', '$']}, // 달러($)
-    {value: ['@@@world@@@', '@']}, // 골뱅이(@)
-    {value: ['###javascript###', '#']}, // 해시(#)
-    {value: ['***code***', '*']}, // 별표(*)
+    {value: [null, undefined]},
+    {value: ['$$$hello$$$', '$']},
+    {value: ['@@@world@@@', '@']},
+    {value: ['###javascript###', '#']},
+    {value: ['***code***', '*']},
 ]
 
 const ITERATIONS = 10000
