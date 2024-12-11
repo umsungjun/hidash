87ff1348050ad644602c7e5508411bf26ac013a7
yceffort <yc.effort@navercorp.com>
2024-12-11 21:17:40 +0900

    [#92] 🔥

--- src/join.ts
+++ src/join.ts
@@ -1,5 +1,3 @@
-import _join from 'lodash/join'
-
 export function join(array: ArrayLike<unknown> | null | undefined, separator?: string): string {
     if (array) {
         return Array.prototype.join.call(array, separator)
