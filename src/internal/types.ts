8b6558940dde93f7555539cb8b35f513b79b5c0b
yujeong-jeon <yujeong.jeon@navercorp.com>
2025-01-02 16:15:55 +0900

    [#57] Many types.ts로 이동

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -5,3 +5,4 @@ export type List<T> = T[] | ArrayLike<T>
 export type Dictionary<T> = Record<string, T>
 export type NumericDictionary<T> = Record<number, T>
 export type AnyKindOfDictionary<T> = Dictionary<T> | NumericDictionary<T>
+export type Many<T> = T | Many<T>[]
