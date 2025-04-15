bb97c683358a362907494a49bc8622ef6fcc6098
sungryong.park <sungryong.park@navercorp.com>
2025-04-15 09:11:02 +0900

    :label: eq return boolean, range return number[]

--- src/range.ts
+++ src/range.ts
@@ -65,7 +65,7 @@ function isIterateeCall(value: number, index: number | undefined, object: Record
     return false
 }
 
-export const range = (start: number, end?: number, step?: number) => {
+export const range = (start: number, end?: number, step?: number): number[] => {
     if (step && typeof step !== 'number' && isIterateeCall(start, end, step)) {
         // eslint-disable-next-line no-param-reassign
         end = step = undefined
