de105fb342c039f83ce7a82e60ccb5970ad14c0e
yujeong-jeon <yujeong.jeon@navercorp.com>
2025-05-07 14:07:26 +0900

    [#103] Update repeat function to handle edge cases for n

--- src/repeat.test.ts
+++ src/repeat.test.ts
@@ -13,6 +13,7 @@ describe('repeat', () => {
         ['abc', -1],
         ['abc', 1.11],
         ['', 3],
+        ['abc', Number.MAX_SAFE_INTEGER + 1], // Exceeds max safe integer
     ])('should repeat the string %o given by n = %d', (str, n) => {
         const expected = _repeat(str, n)
         expect(repeat(str, n)).toBe(expected)
