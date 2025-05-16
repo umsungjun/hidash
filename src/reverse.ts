12bef6d5e5c18bb0d297020406241f90df8d61dc
umsungjun <99730910+umsungjun@users.noreply.github.com>
2025-05-16 16:26:32 +0900

    docs: add JSDoc to reverse function

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,3 +1,10 @@
+/**
+ * Reverses the order of elements in an array `in place` and returns the reversed array.
+ *
+ * @template T
+ * @param {T[]} array - The array to reverse.
+ * @returns {T[]} The same array with elements in reversed order.
+ */
 export function reverse<T>(array: T[]): T[] {
     return Array.prototype.reverse.call(array)
 }
